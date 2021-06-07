034b42d662dac0905093cf5eaa5f787210ff5079
Andreas <info@andreas-heine.net>
2021-06-07 20:05:17 +0200

    update methods

--- README.md
+++ README.md
@@ -12,8 +12,10 @@ prerequirements:
 ### Client:  
 browse (navigate thru the addressspace / node references)  
 read (read node attributes)  
+methods (call a opcua method)  
 write (write node atttibutes) comming soon!  
 subscribe (subscribe different kinds of data) comming soon!  
+extension objects (custom structures)
 client1 (basic client structure) comming soon!  
 client2 (advanced client structure) comming soon!  
 client3 (companion spec.) comming soon!  
--- client/call_method.py
+++ client/call_method.py
@@ -11,13 +11,31 @@ async def main():
     
     method_parent = client.get_node("ns=7;s=Demo.CTT.Methods")
     method_node = client.get_node("ns=7;s=Demo.CTT.Methods.MethodIO") # the method just adds two Int32 numbers
+    method_inputs = client.get_node("ns=7;s=Demo.CTT.Methods.MethodIO.InputArguments")
+    method_ouputs = client.get_node("ns=7;s=Demo.CTT.Methods.MethodIO.OutputArguments")
+
+    # if you dont know the Input-/OutputArguments:
+    inputs = await method_inputs.read_value() # returns a list of Argument-Class
+    for each in inputs:
+        print(each.Name, each.DataType, "-> UInt32")
+    outputs = await method_ouputs.read_value() # returns a list of Argument-Class
+    for each in outputs:
+        print(each.Name, each.DataType, "-> UInt32")
+
+    # sometimes its important to check if a method is executable/userexecutable:
+    executable = await client.uaclient.read_attributes([method_node.nodeid], ua.AttributeIds.Executable)
+    print("Executable: ", executable[0].Value.Value)
+    userexecutable = await client.uaclient.read_attributes([method_node.nodeid], ua.AttributeIds.UserExecutable)
+    print("Userxecutable: ", userexecutable[0].Value.Value)
 
     # always specify the correct VariantType! 
     inarg1 = ua.Variant(20, ua.VariantType.UInt32) 
     inarg2 = ua.Variant(13, ua.VariantType.UInt32)
 
-    result = await method_parent.call_method(method_node, inarg1, inarg2) # the order of args matters!
-    print(result)
+    if executable[0].Value.Value and userexecutable[0].Value.Value:
+        print(f"Calling Method: {inarg1.Value} + {inarg2.Value}")
+        result = await method_parent.call_method(method_node, inarg1, inarg2) # the order of args matters!
+        print("Method Result: ", result)
 
     await client.disconnect()
 
