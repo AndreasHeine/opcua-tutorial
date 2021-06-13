72381160ad748610c2e558a18eef36a512e5f38d
Andreas <info@andreas-heine.net>
2021-06-13 11:49:38 +0200

    add comments

--- client/write.py
+++ client/write.py
@@ -7,7 +7,11 @@ logging.basicConfig(level=logging.WARNING)
 _logger = logging.getLogger('asyncua')
 
 def check_writeable_bit(value):
-    if value & 0b10 > 0:
+    '''
+    https://reference.opcfoundation.org/v104/Core/DataTypes/AccessLevelExType/
+
+    '''
+    if value & 0b10 > 0: # check if bit1 (CurrentWrite) is set
         return True
     return False
 
@@ -17,15 +21,17 @@ async def main():
 
     print("-----------------------------------------------------")
 
-    double_node = client.get_node("ns=2;s=Demo.Static.Scalar.Double")
+    double_node = client.get_node("ns=2;s=Demo.Static.Scalar.Double") # returns a Node-Class
 
     # check if Variable is writeable
-    res = await double_node.read_attribute(ua.AttributeIds.AccessLevel)
+    res = await double_node.read_attribute(ua.AttributeIds.AccessLevel) # returns DataValue-Class
     accesslevel = res.Value.Value
     print("AccessLevel:", accesslevel)
 
     if check_writeable_bit(accesslevel):
         print("The Variable is writeable!")
+        print("-----------------------------------------------------")
+
         # always specify the correct VariantType!
         dv = ua.DataValue(ua.Variant(200.0, ua.VariantType.Double))
         print("Write to the node:")
@@ -38,7 +44,7 @@ async def main():
         print("-----------------------------------------------------")
 
         dv = ua.DataValue(ua.Variant(100.0, ua.VariantType.Double))
-        print("Write to the node:")
+        print("Write to a node by specify the attribute:")
         print(dv)
         await client.uaclient.write_attributes(
             nodeids= [
