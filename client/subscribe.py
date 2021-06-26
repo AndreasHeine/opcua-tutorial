44ff16b93cc98b792c69970313836fd50764d65f
Andreas <info@andreas-heine.net>
2021-06-26 09:10:03 +0200

    Update subscribe.py

--- client/subscribe.py
+++ client/subscribe.py
@@ -9,9 +9,10 @@ _logger = logging.getLogger('asyncua')
 '''
 https://reference.opcfoundation.org/v104/Core/docs/Part4/5.13.1/
 In simple words:
-A OPC UA Subscription works like a "Mailbox" which gets emptied in a defined interval.
-The OPC UA Client sends a publishrequest, the server takes all notifications since the last publishrequest
+A OPC UA Subscription works like a "Mailbox" which gets emptied in a defined interval by the Client.
+The OPC UA Client sends a publishrequest, the server takes all notifications since the last publishrequest.
 and send them in the publishresponse to the client.
+If there is no Notification in the "Mailbox" the Client will get a "keep-alive" back.
 The OPC UA Client can add "MonitoredItems" to the Subscription which will generate Notifications.
 '''
 
@@ -24,7 +25,9 @@ class SubscriptionHandler:
         Callback for asyncua Subscription.
         This method will be called when the Client received a data change message from the Server.
         """
+        # DO NOT DO BLOCKING TASKS HERE , IF YOU NEED TO PROCESS THE DATA PUT IT ON A QUEUE AND PUT IT IN A SUBPROCESS (E.G. SQL QUERYS)!
         print(f"Datachange: {data.monitored_item.Value}")
+
 
 async def main():
     client = Client(url="opc.tcp://127.0.0.1:48010", timeout=4)
@@ -34,12 +37,16 @@ async def main():
     print("-----------------------------------------------------")
 
     nodes = await client.get_node("ns=2;s=Demo.Dynamic.Scalar").get_children() # just get some nodes to subscribe
+
+    # Create a Subscription:
     subscription = await client.create_subscription(
                     period=1000, # the client will send each 1000 ms a publishrequest and the server responds with the changes since last publishrequest
                     handler=handler, # SubscriptionHandler which will be used for processing the notifications in the publishresponse
                     publishing=True
                 )
-    # Reporting            
+
+    # Reporting:
+    # each Attribute change will generate a Notification  
     node_handles = await subscription.subscribe_data_change(
         nodes=nodes, # a list of nodes i want to subscribe to
         attr=ua.AttributeIds.Value, # the attribute i am interested in
@@ -47,7 +54,8 @@ async def main():
         monitoring=ua.MonitoringMode.Reporting
     )
 
-    # Sampling
+    # Sampling:
+    # 
     # TODO
 
     await asyncio.sleep(10)
