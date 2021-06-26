4a0d5b1385521b6b14eeeb227b8941810d6975a8
Andreas <info@andreas-heine.net>
2021-06-26 21:38:56 +0200

    Update client1.py

--- client/client1.py
+++ client/client1.py
@@ -10,26 +10,48 @@
 # RESPECT OPERATIONALLIMITS: READ / WRITE / BROWSE / SUBSCRIBE
 
 import asyncio
+import logging
+from typing import Union, List, Iterable
 from asyncua import Client, ua, Node
 from asyncua.common.subscription import Subscription
+
+logging.basicConfig(level=logging.WARNING)
+_logger = logging.getLogger('asyncua')
+
+class SubscriptionHandler:
+    """
+    The SubscriptionHandler is used to handle the data that is received for the subscription.
+    """
+    async def datachange_notification(self, node: Node, val, data):
+        """
+        Callback for asyncua Subscription.
+        This method will be called when the Client received a data change message from the Server.
+        """
+        # DO NOT DO BLOCKING TASKS HERE , IF YOU NEED TO PROCESS THE DATA PUT IT ON A QUEUE AND PUT IT IN A SUBPROCESS (E.G. SQL QUERYS)!
+        print(f"{node} -> Datachange: {data.monitored_item.Value}")
 
 async def subscribe(
     subscription: Subscription, 
     nodes: Union[Node, Iterable[Node]], 
+    queuesize = 0,
     maxpercall: int = None, 
     attr = ua.AttributeIds.Value,
-    queuesize = 0,
     monitoring = ua.MonitoringMode.Reporting
 ):
     '''
     Subscribing to large numbers of nodes without overwhelming the server!
     '''
+    if not isinstance(nodes, list):
+        nodes = [nodes]
+
     if not maxpercall:
         try:
             maxpercall = await subscription.server.get_node("i=11714").read_value()
         except:
             maxpercall = 0
-    if maxpercall == 0:
+    if maxpercall is None:
+        return await subscription.subscribe_data_change(nodes)
+    elif maxpercall == 0:
         return await subscription.subscribe_data_change(nodes)
     else:
         handle_list = []
@@ -46,3 +68,45 @@ async def subscribe(
 # TODO
 async def unsubscribe():
     pass
+
+async def main():
+    client = Client(url="opc.tcp://127.0.0.1:48010", timeout=4)
+    handler = SubscriptionHandler() # get an instance of the SubscriptionHandler-Class
+    await client.connect()
+
+    print("-----------------------------------------------------")
+    print("checking ServerState")
+    # CHECK SERVERSTATE
+    # TODO
+
+    print("-----------------------------------------------------")
+    print("checking ServiceLevel")
+    # CHECK SERVICELEVEL
+    # TODO
+
+    print("-----------------------------------------------------")
+    print("checking ServerCapabilities")
+    # EXPLORE SERVERCAPABILITIES
+    # TODO
+
+    print("-----------------------------------------------------")
+    print("subscribe to a large number of nodes")
+    nodes = await client.get_node("ns=2;s=Demo.Massfolder_Dynamic").get_variables()
+
+    print(f"{len(nodes)} Nodes found!")
+
+    subscription = await client.create_subscription(
+                    period=5000, # the client will send each 1000 ms a publishrequest and the server responds with the changes since last publishrequest
+                    handler=handler, # SubscriptionHandler which will be used for processing the notifications in the publishresponse
+                    publishing=True
+    )
+
+    handles = await subscribe(subscription=subscription, nodes=nodes, queuesize=100, maxpercall=250)
+
+    await asyncio.sleep(30)
+
+    print("-----------------------------------------------------")
+    await client.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
